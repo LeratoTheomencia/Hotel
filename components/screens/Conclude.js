8cc9b4e42646c19302c9345c634eb12e79f85abf
LeratoTheomencia <leratosebolai.ls@gmail.com>
2022-03-15 10:40:09 +0200

    new-changes

--- components/screens/Conclude.js
+++ components/screens/Conclude.js
@@ -22,7 +22,7 @@ const Conclude = ({ navigation }) => {
     <Text style={styles.text}></Text>
 
     <TouchableOpacity>
-      <Text style={styles.text} onPress={() => navigation.navigate('Home')}>ENJOY YOUR STAY!</Text>
+      <Text style={styles.text} onPress={() => navigation.navigate('Home')}>COMPLETE!</Text>
     </TouchableOpacity>
     </View>
     </View>
